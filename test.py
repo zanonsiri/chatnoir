3def6c8b6d6fdc99e2509ba00d10af28bcf30efe
Iris <iris.zanoncelli@gmail.com>
2021-11-26 22:42:02 +0100

    test 3 commit

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
 __projet__ = "projet_informatique"
-__nom_fichier__ = "test"
+__nom_fichier__ = "test presentation"
 __author__ = "Iris Zanoncelli"
 __date__ = " 26 novembre 2021"
+__version__ = "0.0.1"
