0e9530d0bd3ddd4eb59a4c64be1e5f83cd23da90
Paul Zanoncelli <46530632+parallelepipede@users.noreply.github.com>
2021-11-26 22:39:59 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
 __projet__ = "projet_informatique"
-__nom_fichier__ = "test"
+__nom_fichier__ = "testpluspousse"
 __author__ = "Iris Zanoncelli"
+__team_worker__ = "Emiline Beaudet"
 __date__ = " 26 novembre 2021"
